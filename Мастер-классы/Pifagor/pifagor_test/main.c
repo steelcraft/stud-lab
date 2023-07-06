86b5ea32fce4ee9b65156b13a04785e2ecbd8a07
hachkinaev_ov <ov.hachkinaev@lektronika.org>
2023-07-06 13:58:00 +0300

    Шаг 4

--- Мастер-классы/Pifagor/pifagor_test/main.c
+++ Мастер-классы/Pifagor/pifagor_test/main.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "unity.h"
+#include "pifagor.h"
+//#include "file_to_test.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void test_pifagor_3_and_4_should_return_5(void) {
+    double result = pifagor(3.0, 4.0);
+    TEST_ASSERT_EQUAL_FLOAT(5.0, result);
+}
+
+// not needed when using generate_test_runner.rb
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_pifagor_3_and_4_should_return_5);
+    return UNITY_END();
 }
