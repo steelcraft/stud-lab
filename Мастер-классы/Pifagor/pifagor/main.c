188fb5deaa9190c52831e0cb34020c7233219b18
hachkinaev_ov <ov.hachkinaev@lektronika.org>
2023-07-06 12:49:05 +0300

    Шаг 1

--- Мастер-классы/Pifagor/pifagor/main.c
+++ Мастер-классы/Pifagor/pifagor/main.c
@@ -3,6 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    if (argc != 3){
+        printf("Please specify the sizes of the cathets.\n");
+        return 1;
+    }
+
+    double a, b;
+
+    sscanf(argv[1], "%lf", &a);
+    sscanf(argv[2], "%lf", &b);
+
+    printf("a = %f\t b = %f\n", a, b);
     return 0;
 }
