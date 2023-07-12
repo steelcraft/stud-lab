60293c42b8b6e848d91130291a57aa5545343644
hachkinaev_ov <ov.hachkinaev@lektronika.org>
2023-07-12 12:31:20 +0300

    Добавлен пустой модуль pifagor

---
+++ Мастер-классы/Pifagor-auto/pifagor_auto/include/pifagor.h
@@ -0,0 +1,5 @@
+#ifndef PIFAGOR_H
+#define PIFAGOR_H
+
+
+#endif // PIFAGOR_H
---
+++ Мастер-классы/Pifagor-auto/pifagor_auto/test/Testpifagor.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "pifagor.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_pifagor_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement pifagor");
+}
+
+#endif // TEST
