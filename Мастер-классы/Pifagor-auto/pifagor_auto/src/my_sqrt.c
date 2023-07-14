0b3fe1e4fea92578f9f626e1fe880ebda21eb9ff
hachkinaev_ov <ov.hachkinaev@lektronika.org>
2023-07-14 11:50:00 +0300

    Функция my_sqrt вынесена в отдельный модуль

---
+++ Мастер-классы/Pifagor-auto/pifagor_auto/src/my_sqrt.c
@@ -0,0 +1,5 @@
+#include "my_sqrt.h"
+
+double my_sqrt(double x) {
+    return sqrt(x);
+}
---
+++ Мастер-классы/Pifagor-auto/pifagor_auto/test/Testmy_sqrt.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "my_sqrt.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_my_sqrt_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement my_sqrt");
+}
+
+#endif // TEST
